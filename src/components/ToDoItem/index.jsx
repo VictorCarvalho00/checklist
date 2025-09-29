a939de4ba02d81a53fabf7645fcce482f1c5cbdf
Victor Gabriel <e008732@tst.jus.br>
2025-09-29 10:07:01 -0300

    Adicioando TodoGroup

--- src/components/ToDoItem/index.jsx
+++ src/components/ToDoItem/index.jsx
@@ -1,7 +1,11 @@
 import './todo-item.style.css'
 import { IconPencil, IconTrash } from "../icons";
+import TodoContext from '../TodoProvider/TodoContext';
+import { use } from 'react';
 
-export function ToDoItem ({ item }) {
+export function ToDoItem({ item }) {
+    const { toggleCompleted, deleteTodo } = use(TodoContext)
+
     const styles = ['todo-item']
 
     if (item.completed) {
@@ -14,12 +18,20 @@ export function ToDoItem ({ item }) {
                 {new Date(item.createdAt).toLocaleDateString('pt-BR')}
             </p>
             <div className="details">
-                <input type="checkbox" className="checkbox" defaultChecked={item.completed} />
+                <input
+                    type="checkbox"
+                    className="checkbox"
+                    defaultChecked={item.completed}
+                    onClick={() => toggleCompleted(item)}
+                />
                 <p className="description">
                     {item.description}
                 </p>
                 <div className="actions">
-                    <button className="btn">
+                    <button
+                        className="btn"
+                        onClick={() => deleteTodo(item)}
+                    >
                         <IconTrash />
                     </button>
                     <button className="btn">
